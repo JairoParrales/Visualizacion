cef4a6d3b6a558d456c395805a9e2440e23bf93e
JairoParrales <92266583+JairoParrales@users.noreply.github.com>
2024-08-02 22:48:31 -0500

    Update alumnos_js.js

--- alumnos_js.js
+++ alumnos_js.js
@@ -1,1 +1,1 @@
-[{"alumno":"Jairo","nota":500,"ranking":150},{"alumno":"Alberto","nota":100,"ranking":150}]
+[{"alumno":"Jairo","nota":50,"ranking":150},{"alumno":"Alberto","nota":100,"ranking":150}]
